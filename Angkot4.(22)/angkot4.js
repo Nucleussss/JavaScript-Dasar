19393a95012ab1e9b540eedd14252a41ead48bde
aditya4646 <adityaputraprasetya@gmail.com>
2021-02-19 19:57:08 +0700

    menambah angkot5

--- Angkot4.(22)/angkot4.js
+++ Angkot4.(22)/angkot4.js
@@ -1,10 +1,13 @@
 let jmlAngkot = 10;
     angkotBeroperasi = 6;
     noAngkot = 1;
+    noAngkotLembur = 8;
 
 for ( noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
     if (noAngkot <= angkotBeroperasi) {
         console.log("Angkot No. "+noAngkot+" beroperasi dengan baik.");
+    }else if(noAngkot == noAngkotLembur){
+        console.log("Angkot No. "+noAngkot+" sedang lembur.");
     }else{
         console.log("Angkot No. "+noAngkot+" sedang tidak beroperasi.");
     }
--- Angkot5(23)/angkot5.js
+++ Angkot5(23)/angkot5.js
@@ -1,12 +1,15 @@
 let jmlAngkot = 10;
     angkotBeroperasi = 6;
     noAngkot = 1;
+    noAngkotLembur = [8,10,5];
 
 for ( noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
-    if (noAngkot <= angkotBeroperasi) {
+    if (noAngkot <= angkotBeroperasi && noAngkot !== 5) {
         console.log("Angkot No. "+noAngkot+" beroperasi dengan baik.");
+    }else if(noAngkot == noAngkotLembur[0] || noAngkot == noAngkotLembur[1] || noAngkot == noAngkotLembur[2]){
+        console.log("Angkot No. "+noAngkot+" sedang lembur.");
     }else{
         console.log("Angkot No. "+noAngkot+" sedang tidak beroperasi.");
     }
-}    
+}
 
